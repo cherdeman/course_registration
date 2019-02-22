80f5208db43ab91212386c786fea37d108c8339f
Claire Herdeman <cherdeman@uchicago.edu>
2019-02-22 14:51:27 -0600

    initalizing files from previous assignment

---
+++ person_classes.py
@@ -0,0 +1,48 @@
+# Claire Herdeman
+# OOP: Practicum 1
+# February 11, 2019
+
+# This document is to define mock classes for unit testing
+
+class Person:
+	def __init__(self):
+		pass
+
+class Student:
+	def __init__(self):
+		pass
+
+	def searchCourse(self):
+		pass
+
+class Instructor:
+	def __init__(self):
+		pass
+
+class Course:
+	def __init__(self):
+		pass
+
+class Section:
+	def __init__(self):
+		pass
+
+class Lab:
+	def __init__(self):
+		pass
+
+class Building:
+	def __init__(self):
+		pass
+
+class Room:
+	def __init__(self):
+		pass
+
+class TimeSlot:
+	def __init__(self):
+		pass
+
+class Search:
+	def __init__(self):
+		pass
