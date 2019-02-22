dd4ab24e364c738b2c7cba49d0215167d540a9dd
Claire Herdeman <cherdeman@uchicago.edu>
2019-02-22 15:11:51 -0600

    renaming test script

---
+++ tests/test_people.py
@@ -0,0 +1,46 @@
+# Claire Herdeman
+# OOP: Practicum 1
+# February 11, 2019
+
+# This document contains preliminary unit tests
+import pytest
+
+from mock_classes import *
+
+
+existing_classes = [Person(), Student(), Instructor(), Course(), Section(), Lab(), Building(), Room(), TimeSlot(), Search()]
+person_subclasses = [Student, Instructor]
+course_subclasses = [Section, Lab]
+building_subclasses = [Room]
+
+# Parametrized tests for all classes
+@pytest.mark.parametrize('test_class', existing_classes)
+def test_constructor(test_class):
+	assert len(test_class.__dict__) > 0
+
+@pytest.mark.parametrize('person_class', person_subclasses)
+def test_person_subclass(person_class):
+	assert issubclass(person_class, Person)
+
+@pytest.mark.parametrize('course_class', course_subclasses)
+def test_course_subclass(course_class):
+	assert issubclass(course_class, Course)
+
+@pytest.mark.parametrize('building_class', building_subclasses)
+def test_building_subclass(building_class):
+	assert issubclass(building_class, Building)
+
+# Tests specific to person class
+test_person = Person()
+
+def test_person_name():
+	assert hasattr(test_person, 'name')
+
+def test_person_id():
+	assert hasattr(test_person, '_id')
+
+def test_person_username():
+	assert hasattr(test_person, 'username')
+
+def test_person_password():
+	assert hasattr(test_person, 'password')
